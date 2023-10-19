f33b9ee017516cd6efb605c5397192fe7ef6adb3
deb05121 <129320702+deb05121@users.noreply.github.com>
2023-10-19 22:24:51 +0200

    Largest product project is refactored.

--- largestProduct/src/product/Main.java
+++ largestProduct/src/product/Main.java
@@ -10,30 +10,54 @@ public class Main {
             String value = setNumberLine(scanner);
             System.out.println("Type n:");
             int n = setN(scanner);
-
-            int result = getLargestProduct(value, n);
-            System.out.println(result);
+            boolean isValidN = validator(value, n);
+            int[] numbers = numbersGenerator(value);
+            int result = -1;
+            if (isValidN) {
+                result = getLargestProduct(numbers, n);
+            }
+            System.out.println("result= " + result);
         }
     }
 
-    public static int getLargestProduct(String value, int n) {
-        int max = -1;
-        if (n < 1 || n > value.length()) {
-            return max;
-        }
+    public static int[] numbersGenerator(String value) {
         int[] numbers = new int[value.length()];
         for (int i = 0; i < value.length(); i++) {
             numbers[i] = Integer.parseInt(String.valueOf(value.charAt(i)));
-
         }
-        for (int j = 0; j < value.length() - (n - 1); j++) {
-            int product = 1;
-            for (int k = j; k < j + (n); k++) {
-                product *= numbers[k];
+        return numbers;
+    }
+
+    public static boolean validator(String value, int n) {
+        return n >= 1 && n <= value.length();
+    }
+
+    public static int getLargestProduct(int[] numbers, int n) {
+        int max = -1;
+        double preProduct = 1;
+        for (int j = 0; j < numbers.length - (n - 1); j++) {
+            double product = 1;
+            if (n > 2 && j > 0 && preProduct > 1) {
+                if (numbers[j - 1] != 0) {
+                    product = preProduct / numbers[j - 1];
+                    product *= numbers[j + (n - 1)];
+                } else {
+                    break;
+                }
+            } else {
+                for (int k = j; k < (j + n); k++) {
+                    if (numbers[k] != 0) {
+                        product *= numbers[k];
+                    } else {
+                        product = 0;
+                        break;
+                    }
+                }
             }
             if (product > max) {
-                max = product;
+                max = (int) product;
             }
+            preProduct = product;
         }
         if (max == 0) {
             max = -1;
--- largestProduct/src/product/TestOfLargestProducts.java
+++ largestProduct/src/product/TestOfLargestProducts.java
@@ -8,19 +8,27 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class TestOfLargestProducts {
     @Test
     void testOfProducts() {
-        Assertions.assertEquals(20, Main.getLargestProduct("602144509", 2));
-        Assertions.assertEquals(80, Main.getLargestProduct("602144509", 4));
-        Assertions.assertEquals(160, Main.getLargestProduct("602144509", 5));
-        Assertions.assertEquals(9, Main.getLargestProduct("602144509", 1));
+        int[] numbers = {6,0,2,1,4,4,5,0,9};
+        Assertions.assertEquals(20, Main.getLargestProduct(numbers, 2));
+        Assertions.assertEquals(80, Main.getLargestProduct(numbers, 4));
+        Assertions.assertEquals(160, Main.getLargestProduct(numbers, 5));
+        Assertions.assertEquals(9, Main.getLargestProduct(numbers, 1));
     }
-
 
     @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 6, 10})
+    @ValueSource(ints = {-1, 0, 10})
     void testOfEdgeCases(int number) {
-        Assertions.assertEquals(-1, Main.getLargestProduct("602144509", number));
-
+        String string = "602144509";
+        Assertions.assertFalse(Main.validator(string, number));
     }
+
+    @Test
+    void testNumbersGenerator(){
+        String string = "602144509";
+        int[] numbers = {6,0,2,1,4,4,5,0,9};
+        Assertions.assertArrayEquals(numbers, Main.numbersGenerator(string));
+    }
+
 }
 
 
