aacfc8f111013d1329964c85d603d249694a12c6
deb05121 <129320702+deb05121@users.noreply.github.com>
2023-11-30 18:45:19 +0100

    fix the 3 simple methods

--- simple-methods/src/simple/Main.java
+++ simple-methods/src/simple/Main.java
@@ -1,5 +1,7 @@
 package simple;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -9,15 +11,28 @@ public class Main {
         //1.
         System.out.println("Type a string:");
         boolean isUniqueChars = ThreeEasyMethods.isAllCharactersUnique(scanner.nextLine());
-        System.out.printf("Your string has just unique characters? %s \n",isUniqueChars );
+        System.out.printf("Your string has just unique characters? %s \n", isUniqueChars);
         //2.
         System.out.println("Type the next string:");
         Map<String, Integer> result = ThreeEasyMethods.getWordFrequencies(scanner.nextLine());
         System.out.println(result);
+        //2/a.
+
+        System.out.println("Type some strings. How many words do you want?");
+        int wordNr = scanner.nextInt();
+        scanner.nextLine();
+        List<String> stringList = new ArrayList<>();
+        while (wordNr >0) {
+            stringList.add(scanner.nextLine());
+            wordNr--;
+        }
+        Map<String, Integer> wordsMap = ThreeEasyMethods.getStringFrequencies(stringList);
+        System.out.println(wordsMap);
+
         //3.
         System.out.println("Type a number:");
-        boolean isPrime =  ThreeEasyMethods.isPrime(scanner.nextInt());
-        System.out.printf("Is your number prime? %s",isPrime);
+        boolean isPrime = ThreeEasyMethods.isPrime(scanner.nextInt());
+        System.out.printf("Is your number prime? %s", isPrime);
 
 
     }
--- simple-methods/src/simple/ThreeEasyMethods.java
+++ simple-methods/src/simple/ThreeEasyMethods.java
@@ -13,6 +13,19 @@ public class ThreeEasyMethods {
         return characterSet.size() == len;
     }
 
+    public static Map<String, Integer> getStringFrequencies(List<String> stringList){
+        Map<String, Integer> strFrequencies = new HashMap<>();
+        for (String str: stringList) {
+            String word = str.toLowerCase();
+            if(strFrequencies.containsKey(word)){
+                strFrequencies.put(word, strFrequencies.get(word)+1);
+            }else {
+                strFrequencies.put(word, 1);
+            }
+        }
+        return strFrequencies;
+    }
+
     public static Map<String, Integer> getWordFrequencies(String string) {
         Map<String, Integer> wordAndPieces = new HashMap<>();
         String[] words = string.split(" ");
@@ -29,6 +42,9 @@ public class ThreeEasyMethods {
 
     public static boolean isPrime(int userNumber) {
         int border = (int) Math.sqrt(userNumber) + 1;
+        if(userNumber < 2){
+            return false;
+        }
         for (int i = 2; i < border; i++) {
             if(userNumber%i==0){
                 return false;
